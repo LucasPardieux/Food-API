97d6c9dea28ca61bc84ffdd4668fc6c2df00dc50
lucas alejandro pardieux <lucaspardieux@gmail.com>
2022-06-22 04:31:54 -0300

    Front end vFinal

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -14,7 +14,6 @@ export class Details extends Component {
 
   showHtml() {
     return (<div className={`${style.page}`}>
-      <Link to="/home"><button className={`${style.backButton}`}>Back</button></Link>
       <div className={`${style.contenedor}`}>
         {/* <img className={`${style.imageBackground}`} src={img} alt="background" /> */}
         <img className={`${style.image}`} src={this.props.recipe[0]?.image ? this.props.recipe[0].image : "Loading..."} alt={this.props.recipe[0]?.title} />
@@ -29,17 +28,21 @@ export class Details extends Component {
       </div>
       <div className={`${style.summaryCont}`}>
         <div className={`${style.summary}`}>
-          <h3>{`${this.props.recipe[0]?.summary}` ? `${this.props.recipe[0]?.summary}` : "Loading..."}</h3>
+          {/* <h3>{`${this.props.recipe[0]?.summary}` ? `${this.props.recipe[0]?.summary}` : "Loading..."}</h3> */}
+          <span dangerouslySetInnerHTML={{ __html: this.props.recipe[0]?.summary}}></span>
         </div>
         <div className={`${style.steps}`}>
-        <h3><h1>Steps:</h1>{this.props.recipe[0]?.analyzedInstructions[0]?.steps?.map((s) => { return (<div><h4>Paso numero: {s.number}</h4><h5>{s.step}</h5></div>) })}</h3>
+        <h3><h1>Steps:</h1>{this.props.recipe[0]?.analyzedInstructions[0]?.steps?.map((s) => { return (<div><h4 className={`${style.stepNumber}`}>Paso numero: {s.number}</h4><h5>{s.step}</h5></div>) })}</h3>
         <h3>{this.props.recipe[0]?.analyzedInstructions?.map((s) => {
           return s.number === 0 ? null : (<div><h4>Paso numero: {s.number}</h4><h5>{s.step}</h5></div>)
         })}</h3>
         </div> 
       </div>
+      <Link to="/home"><button className={`${style.backButton}`}>Back</button></Link>
     </div>)
   }
+
+  /*<span className={styles.Summary} dangerouslySetInnerHTML={{ __html: summary}}></span>*/
 
   render() {
     return (
